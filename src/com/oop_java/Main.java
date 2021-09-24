d523b86e2435c49b6787e9472c32306c1f7d613d
Rafex14 <cortesnr@gmail.com>
2021-09-23 20:48:41 -0600

    Segundo trabajo en clase

--- src/com/oop_java/Main.java
+++ src/com/oop_java/Main.java
@@ -1,10 +1,42 @@
 package com.oop_java;
 
+
 public class Main {
+    //JAVA TIENE QUE ESTAR EN CLASES
+    //DECLARAR UN METODO  SIEMPRE TIENEN QUE TENER UNA CLASE  Y PUEDE EXISTIR MUCHOS METODOS EN UNA CLASE
+    static void metodo(){
+
+    }
+
 
     public static void main(String[] args) {
-	// write your code here
+
+        metodo();
+        /*VARIABLES
+            type nombre = valor;
+        * */
+
+        int entero =1;
+        float flotante = 1.5f;
+        String cadenaCaracteres="Entre Dobles Comillas";
+        boolean trueOrFalse = true;
+
+        //Condicionales
+        //if
+        //switch
+
+
+        //Loops
+        //while (i<10){
+        // }
+        //for(){
+        // }
+
+
+        //ARRAYS
+        String[] objetos={"uno","dos"};
 
         System.out.println("Hola Mundo");
+        System.out.println(cadenaCaracteres);
     }
 }
